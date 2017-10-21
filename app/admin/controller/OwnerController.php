f35ae099a7865344965cf7bb0ac1226761998763
s1223s <qin@gxwdbg.com>
2017-10-21 09:36:35 +0800

    完善三级下拉
    前端部分

--- app/admin/controller/OwnerController.php
+++ app/admin/controller/OwnerController.php
@@ -12,30 +12,37 @@ class OwnerController extends AdminBaseController
      public function add()
     {
     	$listObj = db('houses');
-		$whereprovince['level'] = 'xq';
+		$whereprovince['level'] = xq;
 		$listprovince = $listObj->where($whereprovince)->select();
 		//var_dump($listprovince);
 		$this->assign("province_list",$listprovince);
         return $this->fetch();
     }
     	public function get_citys(){
-		$listObj = db('region');
-		$where['top_parentid'] = input('province_id');
-		$where['level'] = 2;
+		$listObj = db('houses');
+		$where['topparentid'] = input('province_id');
+		$where['level'] = d;
 		$list = $listObj->where($where)->select();
 		$data=array('status'=>0,'city'=>$list);
-		//var_dump($list);
-		//$this->assign("city_list",$list);
 		header("Content-type: application/json");
 		exit(json_encode($data));
 	}
 	//获取地级县
 	public function get_district(){
-		$listObj = db('region');
-		$where['parent_id'] = input('city_id');
-		$where['level'] = 3;
+		$listObj = db('houses');
+		$where['parentid'] = input('city_id');
+		$where['level'] = dy;
 		$list = $listObj->where($where)->select();
 		$data=array('status'=>0,'district'=>$list);
+		header("Content-type: application/json");
+		exit(json_encode($data));
+	}
+		public function get_roomid(){
+		$listObj = db('houses');
+		$where['parentid'] = input('district_id');
+		$where['level'] = f;
+		$list = $listObj->where($where)->select();
+		$data=array('status'=>0,'room'=>$list);
 		header("Content-type: application/json");
 		exit(json_encode($data));
 	}
